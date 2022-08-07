d64439d03dcefb463792ed929d766d2f52d3cbf5
goodpaulo <plastra2@gmail.com>
2022-08-07 04:21:35 -0400

    switched BrowserRouter to HashRouter

--- src/RouteSwitch.jsx
+++ src/RouteSwitch.jsx
@@ -1,15 +1,15 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {HashRouter, Routes, Route} from "react-router-dom";
 import App from "./App";
 import StorePage from "./StorePage";
 
 const RouteSwitch = () => {
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/storepage" element={<StorePage />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
